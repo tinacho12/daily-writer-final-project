741d252a03a84c9cd22ca6e032264f1873d68a3f
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-12-03 20:59:35 +0000

    Rails generate devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get("/", { :controller => "submissions", :action => "home" })
+  root to: "submissions#index"
+  #get("/", { :controller => "submissions", :action => "home" })
   get("/write", { :controller => "submissions", :action => "write" })
   post("/submit", { :controller => "submissions", :action => "submit" })
   get("/submissions", { :controller => "submissions", :action => "index" })
